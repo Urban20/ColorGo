e6a98e4e4ca0f9ba09fcda47f8a998ae87dd7bc5
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-06-13 22:17:00 -0300

    se mejora el formateo

--- colorgo.go
+++ colorgo.go
@@ -1,43 +1,48 @@
 package colorgo
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // pequeño codigo para poner colores en consola para el lenguaje de GO
 
-// colores basicos
 const ESCAPE = "\033"
-const ROJO = "0;31"
-const VERDE = "0;32"
-const AMARILLO = "0;33"
-const AZUL = "0;34"
-const CELESTE = "0;36"
-const BLANCO = "0;37"
-const NEGRO = "0;30"
-const VIOLETA = "0;35"
-const RESET = "0"
 
-// subrayados
-const SUB_ROJO = "4;31"
-const SUB_VERDE = "4;32"
-const SUB_AMARILLO = "4;33"
-const SUB_AZUL = "4;34"
-const SUB_CELESTE = "4;36"
-const SUB_BLANCO = "4;37"
-const SUB_NEGRO = "4;30"
-const SUB_VIOLETA = "4;35"
+var colores = map[string]string{
+	// colores basicos
+	"ROJO":     "0;31",
+	"VERDE":    "0;32",
+	"AMARILLO": "0;33",
+	"AZUL":     "0;34",
+	"CELESTE":  "0;36",
+	"BLANCO":   "0;37",
+	"NEGRO":    "0;30",
+	"VIOLETA":  "0;35",
+	"RESET":    "0",
 
-// fondo
-const F_ROJO = "41"
-const F_VERDE = "42"
-const F_AMARILLO = "43"
-const F_AZUL = "44"
-const F_CELESTE = "46"
-const F_BLANCO = "47"
-const F_NEGRO = "40"
-const F_VIOLETA = "45"
+	// subrayados
+	"SUB_ROJO":     "4;31",
+	"SUB_VERDE":    "4;32",
+	"SUB_AMARILLO": "4;33",
+	"SUB_AZUL":     "4;34",
+	"SUB_CELESTE":  "4;36",
+	"SUB_BLANCO":   "4;37",
+	"SUB_NEGRO":    "4;30",
+	"SUB_VIOLETA":  "4;35",
+
+	// fondo
+	"F_ROJO":     "41",
+	"F_VERDE":    "42",
+	"F_AMARILLO": "43",
+	"F_AZUL":     "44",
+	"F_CELESTE":  "46",
+	"F_BLANCO":   "47",
+	"F_NEGRO":    "40",
+	"F_VIOLETA":  "45",
+}
 
 // formateo de colores: ROJO, VERDE, AMARILLO, AZUL, VIOLETA, RESET, BLANCO ...
 func Formateo(color string) string {
 
-	return fmt.Sprintf("%s[%sm", ESCAPE, color)
+	return fmt.Sprintf("%s[%sm", ESCAPE, colores[color])
 }
